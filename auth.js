1401115d298d418eb0e49dbd0853ce11ba7cda4c
errnic3-creator <errnic3@gmail.com>
2026-08-27 11:03:01 +0800

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,7 +1,3 @@
-const SUPABASE_URL = 'https://supabase.com/dashboard/project/ooudzksyaltbhthfulkw';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9vdWR6a3N5YWx0Ymh0aGZ1bGt3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODc3OTM0NTEsImV4cCI6MjEwMzM2OTQ1MX0.8vcqCUsh6rDJQgYbnbGmr12RlmSDC9FsoyHUJTAAV64';
-const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
-
 let isRegister = false;
 
 const tabLogin = document.getElementById('tab-login');
